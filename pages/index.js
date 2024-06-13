ce028efd096b0ca5b503ddc8a3e072f14b9694fd
Murilo Augusto <xoatendimento@gmail.com>
2024-06-13 11:59:58 -0300

    atualizando projeto

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,28 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
 
-
-const inter = Inter({ subsets: ["latin"] });
-
-
+import React from 'react';
+import frutas from '../frutas.json';
 
 export default function Home() {
   return (
-    <h1>HortiFruit</h1>
-)
+    <>
+    <div className={styles.title_container}>
+      <h1 className={styles.title}>
+        Fru<span>tas</span></h1>
+    </div>
+    <div className={styles.frutas_container}>
+    <ul>
+        {frutas.map(item => (
+          <li>
+            <h2>{item.name}</h2>
+            <p>{item.preco}</p>
+            <img src={item.image}/>
+            
+          </li>
+        ))}
+      </ul>
+    </div>
+</>
+  );
 }
